3cdf0695f5ff6e3adae99e32a165e24b824cc054
Christopher Wagner <elementzero23@gmail.com>
2018-12-11 14:15:23 +0100

    Gemeinsames Projekt angelegt

---
+++ Hello.java
@@ -0,0 +1,18 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Hello.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Hello
+{
+    public Hello() {
+        System.out.println("Hello World!");
+    }
+    
+    public static void main(String[] args) {
+        new Hello();
+    }
+
+}
