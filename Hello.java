213ddd738f9c4dc6477f156fb6f88e9d11a4a6e8
Christopher Wagner <elementzero23@gmail.com>
2018-12-11 15:44:52 +0100

    changed system output of class Hello

--- Hello.java
+++ Hello.java
@@ -3,12 +3,12 @@
  * Einfache Hello-World-Ausgabe.
  * 
  * @author Christopher Wagner
- * @version 0.0.1
+ * @version 0.0.2
  */
 public class Hello
 {
     public Hello() {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! 123 Test");
     }
     
     public static void main(String[] args) {
